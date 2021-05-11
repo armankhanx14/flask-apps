4172ea1c72af5ed2a0d6c80a604d8299fb7f5fc4
arman-khan <akhan11@qa.com>
2021-05-11 11:01:27 +0100

    changes made

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-'''
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_internet():
-    return "Hello Internet!"
-
-@app.route('/')
 @app.route('/home')
 def home():
-    return 'this is home page'
+    return 'This is the home page'
+
+@app.route('/about')
+def about():
+    return 'This is the about page'
+
+@app.route('/about/<number>')
+def about_number(number):
+    return 'This is the about page ' + str(number)
 
 
 if __name__=='__main__':
@@ -29,3 +33,4 @@ db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    '''
